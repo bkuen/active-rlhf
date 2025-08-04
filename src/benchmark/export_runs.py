dea764f6dfbb357acf3f917e651a086843cb2636
Benjamin Kuen <info@benjamin-kuen.com>
2025-08-04 20:02:28 +0200

    Update export script

--- src/benchmark/export_runs.py
+++ src/benchmark/export_runs.py
@@ -3,12 +3,20 @@ import wandb
 
 api = wandb.Api()
 # Project is specified by <entity/project-name>
-runs = api.runs("bkuen-ludwig-maximilianuniversity-of-munich/final-benchmark-pusher")
+runs = api.runs("bkuen-ludwig-maximilianuniversity-of-munich/thesis-benchmark-walker")
 
 # Accumulate all rows in a list of dicts
 rows = []
 # exp_names = ["pref_ppo_random"]
-exp_names = ["pref_ppo_random_v1", "pref_ppo_variquery_v1", "pref_ppo_duo_v1"]
+
+# HalfCheetah
+#exp_names = ["prefppo_random__", "prefppo_variquery__", "prefppo_duo_prio_v2__", "prefppo_hybrid_prio_u_v6__"]
+# Hopper
+#exp_names = ["prefppo_random__", "prefppo_variquery_v3__", "prefppo_duo_prio__", "prefppo_hybrid_v3__"]
+# Ant
+#exp_names = ["prefppo_random__", "prefppo_variquery_v10__", "prefppo_duo_prio__", "prefppo_hybrid_prio__"]
+# Walker2d
+exp_names = ["prefppo_random_v2__", "prefppo_variquery_v3__", "prefppo_duo_prio__", "prefppo_hybrid_prio__"]
 
 for run in runs:
     # Check if the run name contains any of the specified experiment names
@@ -33,4 +41,4 @@ print("Saving data to CSV...")
 df = pd.DataFrame(rows, columns=["exp_name", "seed", "charts/episodic_return", "global_step"])
 
 # Save to CSV
-df.to_csv("output/runs_pusher.csv", index=False)
+df.to_csv("output/runs_walker_400.csv", index=False)
